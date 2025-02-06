cac8b92cc8ca1c628ba6d7732d0dde2127684d5d
MouseTrap-codes <mousetrapcodes@gmail.com>
2025-02-06 17:37:33 -0600

    added uthash library

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <htable.h>
+
+yo wheres the htable library, imma do the first pass of parsing
+and adding addresses to labels in hashmap during class rn
+
+check every instruction to check if the number of arguments are correct
+
+increment program counter by 8 for data and 4 for instruction
+
+int main(int argc, char *argv[]) {
+    char buffer[1024];
+    File *file = fopen ("data.txt". "r");
+
+}
